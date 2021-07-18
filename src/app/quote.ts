50f93f54abb136adbc72d5c469bfcfd05c9820d6
Koech-code <nixon.koech@moringaschool.com>
2021-07-19 00:24:18 +0300

    added an upvote and downvote to the blueprint

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showQuotes: boolean;
-    constructor(public author: string, public image: string, public theirQuotes: string, public name: string) {
+    constructor(public author: string, public image: string, public theirQuotes: string, public name: string, public upVote: number, public downVote: number) {
         this.showQuotes = false;
     }
 }
